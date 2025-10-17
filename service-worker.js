837b4e0e0a59ea5c1fdc054b8e83213dd23d06f3
ezequielf566 <ezequielf566@gmail.com>
2025-10-17 05:45:36 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// ✅ Pintando a Palavra — Service Worker (v1.0.9)
+// ✅ Pintando a Palavra — Service Worker (v1.0.11)
 // Corrigido: falha 403 no áudio externo + instalação resiliente
 
 const CACHE_NAME = 'pintando-a-palavra-v1.0.9';
