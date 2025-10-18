aab294529cefd47eb7130dbf71d7ba9e8c53c898
ezequielf566 <ezequielf566@gmail.com>
2025-10-18 05:46:06 -0400

    Update cache name in service worker
    Updated cache name to version 1.0.27.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ✅ Pintando a Palavra — Service Worker (v1.0.20)
 // Corrigido: falha 403 no áudio externo + instalação resiliente
 
-const CACHE_NAME = 'pintando-a-palavra-v1.0.24';
+const CACHE_NAME = 'pintando-a-palavra-v1.0.27';
 const OFFLINE_URL = '/offline.html';
 
 // 🗂️ Lista de arquivos essenciais a serem cacheados
